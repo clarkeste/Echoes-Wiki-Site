64cb78b02754331cd410463a070c130a43744070
Clark Donnelly <clarkdonnelly@Clarks-Mac-mini.local>
2024-06-04 17:49:19 -0500

    Quartz sync: Jun 4, 2024, 5:49 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -15,8 +15,8 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
-    const iconPath = joinSegments(baseDir, "static/icon2.png")
-    const ogImagePath = `https://${cfg.baseUrl}/static/echoes-banner.png`
+    const iconPath = joinSegments(baseDir, "static/icon.png")
+    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
       <head>
@@ -32,10 +32,10 @@ export default (() => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="echoes:banner" content={ogImagePath} />}
+        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="675" />
-        <link rel="icon2" href={iconPath} />
+        <link rel="icon" href={iconPath} />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
         {css.map((href) => (
