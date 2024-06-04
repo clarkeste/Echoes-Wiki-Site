62fd5b4b36ac39d7d432e0ce04059a95903e0f12
Clark Donnelly <clarkdonnelly@Clarks-Mac-mini.local>
2024-06-04 12:24:22 -0500

    Quartz sync: Jun 4, 2024, 12:24 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Email: "Clarkesteban1@gmail.com",
+      "Discord Community": "https://discord.gg/9sugVptV",
     },
   }),
 }
