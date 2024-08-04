31b9303bd27678afcf26c2427743991a8e9fc509
Clark Donnelly <clarkdonnelly@Clarks-Mac-mini.local>
2024-08-04 16:38:23 -0500

    Quartz sync: Aug 4, 2024, 4:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -33,7 +33,7 @@ const config: QuartzConfig = {
           gray: "#282C38",
           darkgray: "#212427",
           dark: "#212427",
-          secondary: "#226F54",
+          secondary: "#008F5D",
           tertiary: "#5FA3AB",
           highlight: "rgba(55, 96, 93, 0.15)",
         },
