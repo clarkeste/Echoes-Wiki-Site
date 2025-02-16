f69098367dfbaa85a043d35bfaa0dd7da83b9f9c
Clark Donnelly <clarkdonnelly@Clarks-Mac-mini.local>
2025-02-16 16:03:54 -0500

    Quartz sync: Feb 16, 2025, 4:03 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,13 +39,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.DesktopOnly(Component.Explorer(({
-      filterFn: (node) => {
-        // Always show folders (unless you want to hide them)
-        if (node.children) return true;
-  
-        // For files: exclude those with "hidden" alias
-        return node.file?.frontmatter?.aliases?.includes("hidden") !== true;
-      },
+  filterFn: (node) => {
+    // exclude files with the tag "hidden"
+    return node.file?.frontmatter?.aliases?.includes("hidden") !== true
+  },
 }))),
   ],
   right: [
@@ -66,11 +63,8 @@ export const defaultListPageLayout: PageLayout = {
     Component.Search(),
     Component.DesktopOnly(Component.Explorer(({
       filterFn: (node) => {
-        // Always show folders (unless you want to hide them)
-        if (node.children) return true;
-  
-        // For files: exclude those with "hidden" alias
-        return node.file?.frontmatter?.aliases?.includes("hidden") !== true;
+        // exclude files with the tag "hidden"
+        return node.file?.frontmatter?.tags?.includes("hidden") !== true
       },
     }))),
   ],
